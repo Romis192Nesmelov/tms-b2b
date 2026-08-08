8762f4ea551769bc7751a8f3b4714fa865792ce1
romis <romis.nesmelov@gmail.com>
2026-08-08 05:40:59 +0300

    changed news layout

--- resources/js/main.js
+++ resources/js/main.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
                 items: 1
             },
             768: {
-                items: 2
+                items: 1
             },
             1200: {
                 items: 4
